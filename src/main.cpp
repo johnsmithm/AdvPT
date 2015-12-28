4a0f0945ef8eac9ef4896fe9d5ec28ba15e60646
t.animal <ar79yxiw@cip.cs.fau.de>
2015-12-28 18:45:01 +0100

    add argument parsing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,68 @@
+#include "Config.h"
 #include "Game.h"
 
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+using namespace std;
+
+void printHelp(po::options_description visibleOpts){
+        cout << "Usage: " << STR(BINARY_NAME) << " [options] build-list\n";
+        cout << visibleOpts << endl;
+}
+
+void printVersion(){
+    cout << STR(BINARY_NAME) << " v" << STR(VERSION_MAJOR) << "." << STR(VERSION_MINOR) << endl;
+}
+
 int main(int argc, char **argv){
+    po::options_description visibleOpts("Options");
+    visibleOpts.add_options()
+        ("help,h", "show this help message and exit")
+        ("version,v", "print the version and exit")
+        ("tech-tree,t", po::value<string>(), "location of the tech tree config file")
+    ;
+
+    po::options_description hiddenOpts;
+    hiddenOpts.add_options()
+        ("build-list", po::value<string>(), "location of the build list to simulate");
+    ;
+
+    po::positional_options_description positionalOpts;
+    positionalOpts.add("build-list", 1);
+
+    po::options_description allOpts;
+    allOpts.add(visibleOpts).add(hiddenOpts);
+
+    po::variables_map vm;
+    try{
+        po::store(
+            po::command_line_parser(argc, argv)
+                .options(allOpts)
+                .positional(positionalOpts)
+                .run(),
+            vm);
+        po::notify(vm);
+    } catch ( const boost::program_options::error& e ) {
+        cerr << e.what() << endl;
+    }
+
+    if (vm.count("help")) {
+        printHelp(visibleOpts);
+        return 0;
+    }
+
+    if(vm.count("version")){
+        printVersion();
+        return 0;
+    }
+
+    if (!vm.count("build-list")){
+        cout << "Missing build-list parameter" << endl << endl;
+        printHelp(visibleOpts);
+        return 0;
+    }
+
     //read config file
     //read buildlist
     //run simulation
