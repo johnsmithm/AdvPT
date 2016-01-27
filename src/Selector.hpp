7ddadd446bf0ae936df582f3d45f7ee2e495e06f
johnsmithm <moshnoi2000@gmail.com>
2016-01-27 20:18:17 +0100

    some correction

--- src/Selector.hpp
+++ src/Selector.hpp
@@ -3,66 +3,86 @@
 
 #include <functional>
 
+#include <vector>
+#include <utility>
+#include "Game.h"
+
+#include "util.h"
+#include "json/json.h"
+#include "fstream"
+
 using namespace std;
 template<typename Gametype>
 class Selector{
 	public:
 		Selector(string mode, string setTarget);
-    void getBestBuildLists(vector<vector<string>>& newlists, vector<vector<string>>& bestLists);
+    	void getBestBuildLists(vector<vector<string>>& newlists, vector<pair<vector<string>, int>>& bestLists);
+    	
 	private:
 		//vector<pair<vector<string>, int>> lastBuildLists;
-
+		int getCompareCriteria(Json::Value output);
 		string mode;
 		string target;
-		std::function<int(int)> getCompareCriteria;
-		int arraySize = 10;
+		size_t arraySize;
+		   
 };
 
-void template<typename Gametype>
-Selector::Selector(string setMode, string setTarget)
-:mode(setMode), target(setTarget)
-{
+template<typename Gametype>
+int Selector<Gametype>::getCompareCriteria(Json::Value output){
 	if(mode == "rush"){
-		getCompareCriteria = [](JsonValue output) {
-			if(output["buildlistValid"] == "1" && output["messages"].size() > 0){
-      	return output["messages"][output["messages"].size() - 1]["time"];
-			}
-			return 1000;
-		};
+				if(output["buildlistValid"] == 1 && output["messages"].size() > 0){
+					Json::Value maX = 0;					
+					return output["messages"][output["messages"].size()-1]["time"].asInt();
+				}
+				return 1000;
 	}
 	else {
-		getCompareCriteria = [](JsonValue output) {
-			if(output["buildlistValid"] == "1" && output["messages"].size() > 0){
+			if(output["buildlistValid"] == 1 && output["messages"].size() > 0){
 				int count = 0;
-      	for(auto message : output["messages"])
-      		for(auto event : message["events"])
-      			if(event["type"] == "build-start" && event["name"] == target)
-      				++count;
-      	return -count;
+				if(output["messages"][output["messages"].size()-1]["time"].asInt() > 360)
+					return 1000;
+				for(auto message : output["messages"])
+					for(auto event : message["events"])
+						if(event["type"] == "build-start" && event["name"] == target)
+							++count;
+				return -count;
 			}
 			return 1000;
-		};
 	}
 }
 
-void template<typename Gametype>
-Selector::getBestBuildLists(vector<vector<string>>& newlists, vector<pair<vector<string>, int>>& bestLists){
+template<typename Gametype>
+Selector<Gametype>::Selector(string setMode, string setTarget)
+:mode(setMode), target(setTarget) ,arraySize(10){}
+
+/**
+ * Simulate newlists, add best list in the bestLists.
+ */
+template<typename Gametype>
+void Selector<Gametype>::getBestBuildLists(vector<vector<string>>& newlists, vector<pair<vector<string>, int>>& bestLists){
 	//lastBuildLists = bestLists;
 	for(auto list : newlists){
 		Gametype g;
 		g.readBuildList(list);//TODO
 		g.simulate();
-		JsonValue output = g.getOutput().getJson();
-		if(output["buildlistValid"] == "1" && output["messages"].size() > 0){
+		Json::Value output = g.getOutput().getJson();
+        g.deleteInstanaces();
+		if(output["buildlistValid"] == 1 && output["messages"].size() > 0){
+			
 			int compareCriteria = getCompareCriteria(output);
 			if(bestLists.size() > arraySize ){
 				if(bestLists[arraySize - 1].second <= compareCriteria)continue;
-				bestLists.erase(bestLists.back());
+				bestLists.erase(bestLists.begin() + arraySize - 1);
+
 			}
+
 			size_t position = bestLists.size();
 			// Find the position where buildList fits(Could use binary search).
 			while(position > 0 && bestLists[--position].second > compareCriteria);
-
+			if(position == 0){
+				ofstream out("output.txt");
+						out<<output;
+			}
 			bestLists.insert(bestLists.begin() + position, make_pair(list,compareCriteria));
 		}
 	}
